25eb4ce5fec83acdb631f06ffd0a02fd09085b3a
Vladyslav <118319413+glavavlada@users.noreply.github.com>
2026-05-04 01:02:39 +0000

    added base footer and header to be worked on for the future. needs to be aligned

--- my-church-site/src/app/layout.tsx
+++ my-church-site/src/app/layout.tsx
@@ -1,9 +1,35 @@
 import "./globals.css";
+import Link from "next/link";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        
+
+        {/* header */}
+        <header className="Header">
+          <h2>Church</h2>
+          {/* navigation */}
+            <nav>
+              <Link href = "/">About</Link>
+              <Link href = "/about">Bible Lessons</Link>
+              <Link href = "/contact">Team</Link>
+              <Link href = "/contact">Events</Link>
+            </nav>
+        </header>
+
+        {/* Footer */}
+        <footer className="Footer">
+          <p>Contact Us at tacomaRussianASD@gmail.com</p>
+        </footer>
+
+
+        {/* Page Countent  not sure how this functions yet but export above doesnt work without*/}
+        {/* keeps content separted from header and footer */}
+        <main>{children}</main>
+     
+      </body>
     </html>
   );
 }

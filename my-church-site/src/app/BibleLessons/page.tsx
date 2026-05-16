845df8cab612a14567d705d89f2900d6fbd5b006
glavavlada <glavavlada13@Gmail.com>
2026-05-15 19:49:53 -0700

    added a way to add embedded website need to style and fix layout but its a start

--- my-church-site/src/app/BibleLessons/page.tsx
+++ my-church-site/src/app/BibleLessons/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+
+import { useState } from "react";
+
 export default function BibleLessons() {
-  return <h1>Bible Lessons </h1>;
+
+  const [showStudent, setShowStudent] = useState(false);
+  const [showTeacher, setShowTeacher] = useState(false);
+
+  return (
+    <div className="lessonPage">
+
+      <h1>Bible Lessons</h1>
+
+      <div className="lessonButtons">
+
+        <button onClick={() => setShowStudent(!showStudent)}>
+          Student Version
+        </button>
+
+        <button onClick={() => setShowTeacher(!showTeacher)}>
+          Teacher Version
+        </button>
+
+      </div>
+
+      {showStudent && (
+        <iframe
+          src="https://www.sabbath.school/SSchool/2026/2/ALQ226_07.pdf"
+          width="100%"
+          height="600px"
+        />
+      )}
+
+      {showTeacher && (
+        <iframe
+          src="https://www.sabbath.school/SSchool/2026/2/ETQ226_07.pdf"
+          width="100%"
+          height="600px"
+        />
+      )}
+
+    </div>
+  );
 }

2b8837bc919937d26eb4fa5c29c24c492c35b2a7
glavavlada <glavavlada13@Gmail.com>
2026-05-18 14:40:51 -0700

    modifed the PDf section.

--- my-church-site/src/app/BibleLessons/page.tsx
+++ my-church-site/src/app/BibleLessons/page.tsx
@@ -4,8 +4,7 @@ import { useState } from "react";
 
 export default function BibleLessons() {
 
-  const [showStudent, setShowStudent] = useState(false);
-  const [showTeacher, setShowTeacher] = useState(false);
+ const [activePDF, setActivePDF] = useState("");
 
   return (
     <div className="lessonPage">
@@ -14,30 +13,59 @@ export default function BibleLessons() {
 
       <div className="lessonButtons">
 
-        <button onClick={() => setShowStudent(!showStudent)}>
+        <button onClick={() => setActivePDF("student")}>
           Student Version
         </button>
 
-        <button onClick={() => setShowTeacher(!showTeacher)}>
+        <button onClick={() => setActivePDF("teacher")}>
           Teacher Version
+        </button>
+
+        <button onClick={() => setActivePDF("student-ru")}>
+          Student RU Version
         </button>
 
       </div>
 
-      {showStudent && (
+      {activePDF === "student" && (
+        <div className="pdfSection">
+
+          <h2>Student Version</h2>
+
         <iframe
           src="https://www.sabbath.school/SSchool/2026/2/ALQ226_07.pdf"
           width="100%"
           height="600px"
         />
+
+        </div>
       )}
 
-      {showTeacher && (
+      {activePDF === "teacher" && (
+        <div className="pdfSection">
+
+          <h2>Teacher Version</h2>
+
+          <iframe
+            src="https://www.sabbath.school/SSchool/2026/2/ETQ226_07.pdf"
+            width="100%"
+            height="600px"
+          />
+        </div>
+      )}
+
+      {activePDF === "student-ru" && (
+        <div className="pdfSection">
+
+          <h2>Student RU Version</h2>
+
         <iframe
-          src="https://www.sabbath.school/SSchool/2026/2/ETQ226_07.pdf"
+          src="/Субботняя_школа_May15_2026.pdf"
           width="100%"
           height="600px"
         />
+
+        </div>
       )}
 
     </div>

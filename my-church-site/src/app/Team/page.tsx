1ba9d5b95c6e9e904d13c7bdd24ce00f7c3c67d7
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2026-06-28 16:47:15 -0700

    trimmed position lists

--- my-church-site/src/app/Team/page.tsx
+++ my-church-site/src/app/Team/page.tsx
@@ -9,10 +9,6 @@ const teamGroups: TeamGroup[] = [
     members: ["Vitalii Glavatskyi"]
   },
   {
-    role: "Elder",
-    members: ["Serhii Kostiuk", "Ivan Bokov", "Maksym Briukhovets"]
-  },
-  {
     role: "Clerk",
     members: ["Dianna Croitor"]
   },
@@ -21,72 +17,16 @@ const teamGroups: TeamGroup[] = [
     members: ["Ala Cleofas"]
   },
   {
-    role: "Deacon - Head",
-    members: ["Victor Kasap"]
+    role: "Elder",
+    members: ["Serhii Kostiuk", "Ivan Bokov", "Maksym Briukhovets"]
   },
   {
     role: "Deacon",
-    members: ["Valerii Croitor", "Illia Kostiuk", "Pavlo Kostiuk", "Valentin Lala", "Timur Lechiev", "Maxim Lukin", "Oleksandr Shevchuk", "Vialii Vyshnevskyi"]
-  },
-  {
-    role: "Deaconess - Head",
-    members: ["Maria Kasap"]
+    members: ["Victor Kasap", "Valerii Croitor", "Illia Kostiuk", "Pavlo Kostiuk", "Valentin Lala", "Timur Lechiev", "Maxim Lukin", "Oleksandr Shevchuk", "Vialii Vyshnevskyi"]
   },
   {
     role: "Deaconess",
-    members: ["Tatiana Croitor", "Irina Gerasimchuk", "Yelena Kuzmina", "Liudmila Lala", "Elena Pindiurina", "Irina Pindiurina", "Elena Railean", "Lesya Voroniuk"]
-  },
-  {
-    role: "Building Mgr",
-    members: ["Pavlo Kostiuk", "Vasili Cleofas", "Valerii Croitor", "Leonid Lala", "Valentin Lala", "Gheorghe Sirbu"],
-  },
-  {
-    role: "Chilren's Min Dir",
-    members: ["Yelena Kuzmina"],
-  },
-  {
-    role: "Children's Min Dir Asst",
-    members: ["Tatiana Croitor"],
-  },
-  {
-    role: "Family Min Coord",
-    members: ["Ivan Melniciuc", "Larisa Melniciuc"],
-  },
-  {
-    role: "Finance Comm - Chair",
-    members: ["Eugeniu Baidiuc"],
-  },
-  {
-    role: "Finance Officer",
-    members: ["Irina Gerasimchuk", "Lilia Lala", "Svitlana Shevchuk", "Lesya Varoniuk"],
-  },
-  {
-    role: "Health/Temp Dir",
-    members: ["Natalia Bakhtala"],
-  },
-  {
-    role: "Kitchen Ministry",
-    members: ["Elena Railean"],
-  },
-  {
-    role: "Librarian",
-    members: ["Elena Pindiurina"],
-  },
-  {
-    role: "Master Guide Coord",
-    members: ["Andrey Glavatskyy"],
-  },
-  {
-    role: "Media Min Coord",
-    members: ["Reshat Sitdikov", "Vlad Glavatskyy"],
-  },
-  {
-    role: "Music Coord",
-    members: ["Liliya Sitdikova"],
-  },
-  {
-    role: "Musician",
-    members: ["Tetiana Kostiuk", "Vildan Sitdikov", "Victor Kasap","Serghei Railean"],
+    members: ["Maria Kasap","Tatiana Croitor", "Irina Gerasimchuk", "Yelena Kuzmina", "Liudmila Lala", "Elena Pindiurina", "Irina Pindiurina", "Elena Railean", "Lesya Voroniuk"]
   },
 ];
 

ec159f0fd8d79d3ffac11864f9a42ff1896fe885
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2026-07-10 15:41:45 -0700

    added consisten h1 styling

--- my-church-site/src/app/Team/page.tsx
+++ my-church-site/src/app/Team/page.tsx
@@ -26,24 +26,27 @@ const teamGroups: TeamGroup[] = [
   },
   {
     role: "Deaconess",
-    members: ["Maria Kasap","Tatiana Croitor", "Irina Gerasimchuk", "Yelena Kuzmina", "Liudmila Lala", "Elena Pindiurina", "Irina Pindiurina", "Elena Railean", "Lesya Voroniuk"]
+    members: ["Maria Kasap", "Tatiana Croitor", "Irina Gerasimchuk", "Yelena Kuzmina", "Liudmila Lala", "Elena Pindiurina", "Irina Pindiurina", "Elena Railean", "Lesya Voroniuk"]
   },
 ];
 
 export default function Team() {
   return (
-    <section className="teamGrid">
-      {teamGroups.map((group) => (
-        <div key={group.role} className="teamColumn">
-          <h3>{group.role}</h3>
+    <section className="teamPage">
+      <h1>Team</h1>
+      <section className="teamGrid">
+        {teamGroups.map((group) => (
+          <div key={group.role} className="teamColumn">
+            <h3>{group.role}</h3>
 
-          <ul>
-            {group.members.map((member) => (
-              <li key={member}>{member}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+            <ul>
+              {group.members.map((member) => (
+                <li key={member}>{member}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </section>
     </section>
   );
 }

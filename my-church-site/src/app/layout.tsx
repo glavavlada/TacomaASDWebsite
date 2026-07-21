02728bd5278bb68990a2bdcb0ab3074df6d5fc7a
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2026-07-21 13:51:06 -0700

    tailwind demonstration

--- my-church-site/src/app/layout.tsx
+++ my-church-site/src/app/layout.tsx
@@ -7,7 +7,7 @@ import { LanguageProvider } from "@/app/context/LanguageContext";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="pages">
+      <body className="min-h-screen flex flex-col">
         <LanguageProvider>
           <Navbar />
 

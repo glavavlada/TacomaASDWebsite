904994f553615a7d434d491ef9e8dc724cc67187
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2026-08-03 14:54:30 -0700

    added "standard sticky footer"

--- my-church-site/src/app/layout.tsx
+++ my-church-site/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
           {/* Page Countent  not sure how this functions yet but export above doesnt work without*/}
           {/* keeps content separted from header and footer */}
-          <main className="self-center w-full max-w-[80rem] min-h-screen px-6 py-8 lg:px-36">
+          <main className="flex-1 self-center w-full max-w-[80rem] px-6 py-8 lg:px-36">
             {children}
           </main>
 

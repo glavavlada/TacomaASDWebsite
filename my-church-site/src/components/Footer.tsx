01e3c711d9c69cf95f92e5f23c5fe821cbfabe34
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2026-08-05 15:26:57 -0700

    changed css to tailwind and a universal link style

--- my-church-site/src/components/Footer.tsx
+++ my-church-site/src/components/Footer.tsx
@@ -1,21 +1,20 @@
 "use client";
 import { useState } from "react";
-import Link from "next/link";
 import Donations from "./Donations";
 
 export default function Foot() {
   const [showDonate, setShowDonate] = useState(false);
   return (
     <>
-      <footer className="Footer">
+      <footer className="p-4 bg-[var(--background)] flex justify-between items-center">
         <p>9241 S D St <br></br>Tacoma, WA 98444 <br></br>United States</p>
 
-        <div className="contactSection">
+        <div className="text-center">
 
           <p>
             <a
               href="mailto:tacomaRussianASD@gmail.com"
-              className="footerLink"
+              className="link"
             >Email us at tacomaRussianASD@gmail.com!</a>
           </p>
 
@@ -24,7 +23,7 @@ export default function Foot() {
               href="https://www.facebook.com/tacomarussian/"
               target="_blank"
               rel="noopener noreferrer"
-              className="footerLink"
+              className="link"
             >
               Find us on Facebook!
             </a>
@@ -32,7 +31,7 @@ export default function Foot() {
         </div>
 
         <button
-          className="donateButton"
+          className="buttonLight"
           onClick={() => setShowDonate(true)}
         >
           Donate

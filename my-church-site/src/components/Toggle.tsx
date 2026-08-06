f129fd69bc7deb4c771a10fab8d7be6e62fc7e24
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2026-08-06 16:46:14 -0700

    added toggle.css for animations and changed some logic to tailwind

--- my-church-site/src/components/Toggle.tsx
+++ my-church-site/src/components/Toggle.tsx
@@ -13,13 +13,13 @@ export default function Toggle({
 }: ToggleProps) {
     return (
         <button
-            className={`toggle ${value ? "right" : "left"}`}
+            className={`relative flex cursor-pointer overflow-hidden rounded-full border-2 border-[var(--earth)] bg-[var(--buttonDark)] p-1 hover:bg-[var(--hoverDark)] ${value ? "right" : "left"}`}
             onClick={() => onChange(!value)}
             type="button"
         >
             <span className="slider" />
-            <span className="option">{left}</span>
-            <span className="option">{right}</span>
+            <span className="relative z-10 px-2">{left}</span>
+            <span className="relative z-10 px-2">{right}</span>
         </button>
     );
 }

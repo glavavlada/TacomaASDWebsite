2863c36f51546fd4b898f6bf8e2dfbb912261992
Vladyslav <glavavlada13@Gmail.com>
2026-08-07 00:14:19 -0700

    fixed the team translation for russian

--- my-church-site/src/app/Team/page.tsx
+++ my-church-site/src/app/Team/page.tsx
@@ -1,14 +1,31 @@
-import teamGroups from "@/locale/en/team.json";
+"use client";
+
+import englishTeam from "@/locale/en/team.json";
+import russianTeam from "@/locale/ru/team.json";
+
+import { useLanguage } from "@/app/context/LanguageContext";
 
 export default function Team() {
+  const { language } = useLanguage();
+
+  const data =
+    language === "en"
+      ? englishTeam
+      : russianTeam;
+
   return (
     <section>
-      <h1>Team</h1>
+      <h1>{data.labels.pageTitle}</h1>
 
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {teamGroups.map((group) => (
-          <div key={group.role} className="text-center">
-            <h3 className="mb-3 bg-[var(--earth)] p-2">{group.role}</h3>
+        {data.groups.map((group) => (
+          <div
+            key={group.role}
+            className="text-center"
+          >
+            <h3 className="mb-3 bg-[var(--earth)] p-2">
+              {group.role}
+            </h3>
 
             <ul>
               {group.members.map((member) => (

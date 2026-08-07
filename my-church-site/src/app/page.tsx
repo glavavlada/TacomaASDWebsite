b91e2f2cc0a7343d44f9cf86323e9ba3ed023e6d
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2026-08-07 14:36:09 -0700

    switched text sections in about and removed non-main deacons

--- my-church-site/src/app/page.tsx
+++ my-church-site/src/app/page.tsx
@@ -11,8 +11,8 @@ export default function About() {
     <div className="aboutPage">
       <LiveStreamEmbed />
 
-      {/* History Section */}
-      <h1>History</h1>
+      {/* Beliefs Section */}
+      <h1>Beliefs</h1>
       <section className="infoSection">
         <section className="imgWrapper">
           <Image
@@ -30,27 +30,6 @@ export default function About() {
 
         <div className="textContent">
           <p>
-            Our journey began in 2004 with the establishment of the southern branch of the Center for Spiritual Enrichment in Tacoma. Under the leadership of our first pastor, Ivan Bokov, the congregation flourished, offering spiritual nourishment and fellowship to many.
-            In 2009, the congregation was officially recognized as a church, marking a significant milestone in our journey.
-            In 2015, guided by faith and determination, the church took a bold step forward by purchasing a building to serve as our permanent place of worship.
-            The culmination of years of dedication and faith came in 2024 with the consecration of our prayer house.
-            Also in 2024, Pastor Vitalii Glavatskiy took over as the head pastor of the church, while Ivan Bokov remains as an elder to share his experience.
-          </p>
-          <p>
-            Today, our church stands as a vibrant community of believers, united in our mission to share the gospel and demonstrate God&apos;s love to all.
-            Among our members are Russians, Ukrainians, Moldovans, Gagauz, Belorussians, Romanians, Rusins, Armenians, Uzbeks, Turkmens, and Americans, each bringing unique traditions and cultural heritage.
-            This cultural richness fosters unity and mutual respect. Together, we worship as one body, celebrating our shared faith while embracing our differences.
-            Over the years, our church has expanded in numbers, mission, and impact. We strive to be a beacon of hope and love, offering ministries that serve both spiritual and practical needs. Our worship services, Bible studies, and outreach initiatives aim to draw people closer to Christ and one another.
-          </p>
-
-        </div>
-      </section>
-
-      {/*Beliefs Section*/}
-      <h1>Beliefs</h1>
-      <section className="infoSection">
-        <div className="textContent">
-          <p>
             We are Seventh-day Adventists, Christians who strive to live according to biblical principles, following the teachings of Jesus Christ.
             At the center of our faith is God&apos;s love, revealed through Christ, who died for our sins and rose again to give us the hope of eternal life. We believe that the Bible is the Word of God, the only true source of truth, and a guide for our lives.
             A special part of our faith is the seventh day of the week—Sabbath—which was established by God as a day of rest, worship, and spiritual renewal.
@@ -62,6 +41,26 @@ export default function About() {
             We also place great importance on a healthy lifestyle because our bodies are the temple of the Holy Spirit. We promote balanced living, emphasizing the importance of mental, emotional, and physical well-being. By maintaining good health, we honor God and better equip ourselves to serve others.
             We engage in educational, medical, and charitable projects, striving to benefit society and inspire those around us. We are a large spiritual family, united by faith, hope, and the desire to serve God and people.
             Our doors are always open to those searching for truth, meaning, and connection. If you are looking for a place where you can grow spiritually, find purpose, and receive support, we would be happy to welcome you among us!
+          </p>
+        </div>
+      </section>
+
+      {/*History Section*/}
+      <h1>History</h1>
+      <section className="infoSection">
+        <div className="textContent">
+          <p>
+            Our journey began in 2004 with the establishment of the southern branch of the Center for Spiritual Enrichment in Tacoma. Under the leadership of our first pastor, Ivan Bokov, the congregation flourished, offering spiritual nourishment and fellowship to many.
+            In 2009, the congregation was officially recognized as a church, marking a significant milestone in our journey.
+            In 2015, guided by faith and determination, the church took a bold step forward by purchasing a building to serve as our permanent place of worship.
+            The culmination of years of dedication and faith came in 2024 with the consecration of our prayer house.
+            Also in 2024, Pastor Vitalii Glavatskiy took over as the head pastor of the church, while Ivan Bokov remains as an elder to share his experience.
+          </p>
+          <p>
+            Today, our church stands as a vibrant community of believers, united in our mission to share the gospel and demonstrate God&apos;s love to all.
+            Among our members are Russians, Ukrainians, Moldovans, Gagauz, Belorussians, Romanians, Rusins, Armenians, Uzbeks, Turkmens, and Americans, each bringing unique traditions and cultural heritage.
+            This cultural richness fosters unity and mutual respect. Together, we worship as one body, celebrating our shared faith while embracing our differences.
+            Over the years, our church has expanded in numbers, mission, and impact. We strive to be a beacon of hope and love, offering ministries that serve both spiritual and practical needs. Our worship services, Bible studies, and outreach initiatives aim to draw people closer to Christ and one another.
           </p>
         </div>
 

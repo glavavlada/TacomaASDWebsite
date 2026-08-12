eabb7794488db202b6d9375c3a64272d92410910
Vladyslav <glavavlada13@Gmail.com>
2026-08-11 19:49:32 -0700

    added intro to about page with a picture

--- my-church-site/src/app/page.tsx
+++ my-church-site/src/app/page.tsx
@@ -6,6 +6,9 @@ import Image from "next/image";
 import englishAbout from "@/locale/en/about.json";
 import russianAbout from "@/locale/ru/about.json";
 
+import englishIntro from "@/locale/en/Intro.json";
+import russianIntro from "@/locale/ru/Intro.json";
+
 import { useLanguage } from "@/app/context/LanguageContext";
 
 const churchImgOut = "/church_outside.jpg";
@@ -19,9 +22,49 @@ export default function About() {
       ? englishAbout
       : russianAbout;
 
+  const introData =
+    language === "en"
+      ? englishIntro
+      : russianIntro;
+
   return (
     <div className="aboutPage">
       <LiveStreamEmbed />
+
+      {/*intro/welcome section */}
+      <section
+        className=" relative mb-12 min-h-[500px] overflow-hidden bg-cover bg-center flex items-center"
+        style={{ backgroundImage: "url('/IntroPic.jpg')" }}
+      >
+        {/* transparent text box */}
+        <div
+          className=" ml-12 max-w-2xl rounded-xl bg-[color:var(--buttonDark)]/85 p-8 text-white backdrop-blur-[2px] "
+        >
+          <h1 className="mb-2 font-semibold">
+            {introData.intro.title}
+          </h1>
+
+          <h2 className="mb-5">
+            {introData.intro.subtitle}
+          </h2>
+
+          <div className="flex flex-col gap-3 leading-6">
+            {introData.intro.paragraphs.map((paragraph, index) => (
+              <p key={index}>{paragraph}</p>
+            ))}
+          </div>
+
+          <p className="mt-5 font-semibold">
+            {introData.intro.address}
+          </p>
+
+          <p className="mt-4">
+            {introData.intro.closing}
+          </p>
+        </div>
+      </section>
+
+
 
       {/* Beliefs Section */}
       <h1>{data.labels.beliefsTitle}</h1>

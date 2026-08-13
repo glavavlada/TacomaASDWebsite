72825203e95228aa85f4afe570ba089b3db65f77
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2026-08-13 16:46:31 -0700

    redeployment test

--- my-church-site/src/app/layout.tsx
+++ my-church-site/src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <LanguageProvider>
         <Navbar />
 
-        {/* Page Countent  not sure how this functions yet but export above doesnt work without*/}
         {/* keeps content separted from header and footer */}
         <main className="[background-color:var(--body)] text-[var(--textDark)] flex-1 mx-auto w-full px-[clamp(1rem,10vw,20rem)] py-8">
           {children}

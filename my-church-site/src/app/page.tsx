279eb94e5e3e8bf19de6ad576844bbb8fa459f09
Evgeniia Nemynova <evgeniianemynova@gmail.com>
2026-08-20 16:39:11 -0700

    darkened intro and cleaned up code

--- my-church-site/src/app/page.tsx
+++ my-church-site/src/app/page.tsx
@@ -40,7 +40,7 @@ export default function About() {
       >
         {/* Makes the left side darker so the text stays readable */}
         <div
-          className=" absolute inset-0 bg-gradient-to-r from-black/70 via-black/25 to-transparent"
+          className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/40 to-transparent"
         />
 
         {/* Intro text */}

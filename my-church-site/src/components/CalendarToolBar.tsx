c4a4e113085828cd1d816a946d6850aa8ce0cff1
Vladyslav <glavavlada13@Gmail.com>
2026-09-20 14:17:02 -0700

    fixed error with calendar not registering goToToday

--- my-church-site/src/components/CalendarToolBar.tsx
+++ my-church-site/src/components/CalendarToolBar.tsx
@@ -40,6 +40,7 @@ export default function CalendarToolbar({
                 <button
                     onClick={goToNextMonth}
                     className="buttonDark"
+
                     aria-label="Next month"
                 >
                     →
